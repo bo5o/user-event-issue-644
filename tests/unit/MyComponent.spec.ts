fe8419819aa349fc58b275d2cc9c8c3616b0a271
cbows <32486983+cbows@users.noreply.github.com>
2021-05-02 19:12:45 +0200

    add code to reproduce issue

---
+++ tests/unit/MyComponent.spec.ts
@@ -0,0 +1,14 @@
+import { screen, render } from "@testing-library/vue";
+
+import MyComponent from "@/components/MyComponent.vue";
+import userEvent from "@testing-library/user-event";
+
+describe("MyComponent.vue", () => {
+  it("can change value", async () => {
+    render(MyComponent);
+
+    const input = screen.getByPlaceholderText("some field");
+
+    await userEvent.type(input, "abcd", { delay: 50 });
+  });
+});
